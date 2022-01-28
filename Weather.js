3325528bc10eaed373ba81587691aa64f0ce5a2b
Qingkezou <qingkezou@gmail.com>
2022-01-27 16:22:52 -0800

    draft 1 finished

--- Weather.js
+++ Weather.js
@@ -14,12 +14,16 @@ function NewElem(current_id){
         element.appendChild(text)
     }
     }
-new NewElem('city').createNew()
-new NewElem('time').createNew()
-new NewElem('temp').createNew()
-new NewElem('condition').createNew()
-new NewElem('feelslike').createNew()
 
+new NewElem('city').createNew()
+new NewElem('condition').createNew()
+new NewElem('icon').createNew()
+new NewElem('temp').createNew()
+new NewElem('line').createNew()
+new NewElem('time').createNew()
+
+new NewElem('feelslike').createNew()
+new NewElem('humidity').createNew()
 
 
 function handleSubmit(e){
@@ -33,6 +37,7 @@ async function fetchWeb (where){
         let  response= await fetch(`https://api.weatherapi.com/v1/current.json?key=3ff5b41e1989466dbfc03436222301&q=${where}`, {mode: 'cors'})
         var web=await response.json()
         console.log(web)
+        show_reset()
         
         
         //ending point
@@ -58,8 +63,11 @@ function ProcessData(web){
      city: web.location.name.toUpperCase(),
      time:web.location.localtime,
      temp:Math.round(web.current.temp_f),
+     is_day:web.current.is_day,
      condition:web.current.condition.text,
-     feellike:web.current.feelslike_f
+     
+     feellike:Math.round(web.current.feelslike_f),
+     humidity:web.current.humidity,
 
     }
 
@@ -74,19 +82,54 @@ return data
 
 
  function Result(data){
+    // var title_city=document.getElementById('Title_city')
+    // var title_time=document.getElementById('Title_time')
+    // var title_temp=document.getElementById('Title_temp')
+    // var title_cond=document.getElementById('Title_condition')
+    
+
     var city=document.getElementById('city')
     var time=document.getElementById('time')
+    
     var temp=document.getElementById('temp')
     var cond=document.getElementById('condition')
     var fl=document.getElementById('feelslike')
+    var line=document.getElementById('line')
+    var humidity=document.getElementById('humidity')
+    
+    // title_time.innerHTML="Local Time: "
+    // title_temp.innerHTML="Current Temperature: "
+    // title_cond.innerHTML="Current Condition: "
+    // title_fl.innerHTML="Feels like: "
+
+
     city.innerHTML=data.city
-    time.innerHTML=data.time
     temp.innerHTML=data.temp
     cond.innerHTML=data.condition
-    fl.innerHTML=data.feellike
+    fl.innerHTML="Feels like: "+ data.feellike
+    humidity.innerHTML="Humidity: "+data.humidity
     
 
+    //change background pic
+    day_night_background(data)
+    
 
 
 }
 
+function show_reset(){
+    var result=document.getElementById('Result')
+     result.style.display="flex";
+     form.reset()
+}
+
+function day_night_background(data){
+    if(data.is_day==0){
+    document.body.style.background= "url('night.jpg') no-repeat center center fixed";
+    document.body.style.backgroundSize="cover"
+    }else{
+    document.body.style.background= "url('daytime.jpg') no-repeat center center fixed";
+    document.body.style.backgroundSize="cover"
+
+    }
+}
